4e1ac8669f5b39182310307c3096b8ffac0ba871
ibrahim <ib.arshad777@gmail.com>
2021-02-20 21:55:32 +0600

    Initial publish

---
+++ avro.worker.js
@@ -0,0 +1,41 @@
+importScripts("/avro.min.js");
+importScripts("/comlink.min.js");
+
+var CandidateSelection = {};
+
+var avro = new AvroPhonetic(
+  function () {
+    return CandidateSelection;
+  },
+  function (cS) {
+    CandidateSelection = cS;
+  }
+);
+
+var comm = {
+  getVersion: function () {
+    return avro.version;
+  },
+  getSuggestion: function (subtext) {
+    var res = subtext.match(/\s?([^\s]+)$/);
+    if (res == null) return null;
+    var bnregex = /[\u0980-\u09FF]+$/;
+    if (bnregex.exec(res[1])) return null;
+    var query = res[1];
+
+    var bnarr = avro.suggest(query);
+
+    bnarr.words = bnarr.words.slice(0, 10);
+    if (avro.candidate(query) === query) {
+      bnarr.prevSelection = bnarr.words.length;
+    }
+    bnarr.words.push(query);
+
+    return bnarr;
+  },
+  setCommit: function (qtxt, value) {
+    avro.commit(qtxt, value);
+  }
+}
+
+Comlink.expose(comm);
---
+++ precache-manifest.9f74d5472a11e6b82c696ffdc495fe05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d61d0623e524d49ef58ba4069a4e1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8aaf93e44e5279fbfb01",
+    "url": "/static/css/main.456e743e.chunk.css"
+  },
+  {
+    "revision": "411cf22a8d2ab975d5d0",
+    "url": "/static/js/2.152de90f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.152de90f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aaf93e44e5279fbfb01",
+    "url": "/static/js/main.fb7cafea.chunk.js"
+  },
+  {
+    "revision": "d5c1b9abed4738a5f1ec",
+    "url": "/static/js/runtime-main.c715e46b.js"
+  }
+]);
