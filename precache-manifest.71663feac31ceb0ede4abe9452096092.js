6c31d56b3e1b113ef14c10893b016444e248e002
ibrahim <ib.arshad777@gmail.com>
2021-02-20 22:03:16 +0600

    Updated public url

---
+++ precache-manifest.71663feac31ceb0ede4abe9452096092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc802afccf8011c60b0eae6b4dd72fb",
+    "url": "/avro-writer/index.html"
+  },
+  {
+    "revision": "fa50416d493e518ac361",
+    "url": "/avro-writer/static/css/main.456e743e.chunk.css"
+  },
+  {
+    "revision": "411cf22a8d2ab975d5d0",
+    "url": "/avro-writer/static/js/2.152de90f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/avro-writer/static/js/2.152de90f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa50416d493e518ac361",
+    "url": "/avro-writer/static/js/main.27b69d3e.chunk.js"
+  },
+  {
+    "revision": "222dab8ae4f073dd895e",
+    "url": "/avro-writer/static/js/runtime-main.62bc3877.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f74d5472a11e6b82c696ffdc495fe05.js"
+  "/avro-writer/precache-manifest.71663feac31ceb0ede4abe9452096092.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/avro-writer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
