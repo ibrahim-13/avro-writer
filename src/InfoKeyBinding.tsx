9d6901949704d3fc2a1039816494d96234fe9254
ibrahim <ib.arshad777@gmail.com>
2021-02-22 23:12:14 +0600

    Add kbd tag style

--- src/InfoKeyBinding.tsx
+++ src/InfoKeyBinding.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function InfoKeyBinding(): JSX.Element {
   return (
     <div className="info-keybinding">
-      (CTRL + . to Toggle Language, arrow keys to traverse suggestions, enter to apply suggestion)
+      (<kbd>CTRL</kbd> + <kbd>.</kbd> to Toggle Language, <kbd>&larr;</kbd> and <kbd>&rarr;</kbd> to traverse suggestions, <kbd>ENTER</kbd> to apply suggestion, <kbd>ESC</kbd> to clear suggestions)
     </div>
   );
 }
